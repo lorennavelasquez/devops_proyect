41bb2d76d313c90fb72e454a603fb9c449f8be69
lorenna velasquez <lorennavelasquez@MacBook-Air-de-lorenna.local>
2026-02-27 10:20:39 -0500

    Configuración inicial del proyecto web para Trunk-Based Development

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('action-btn').addEventListener('click', function() {
+    alert('¡Hola, profesor! Estos 3 archivos fueron integrados rápidamente a la rama principal usando Trunk-Based Development.');
+});
