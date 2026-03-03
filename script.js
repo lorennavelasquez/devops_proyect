f1a168dbec894a630c29fb8601d6753aed0d02a3
DaniFigueroa11 <figueroa.dani0406@gmail.com>
2026-03-03 17:39:01 -0500

    Actualizamos el codigo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById('action-btn').addEventListener('click', function() {
     alert('¡Hola, profesor! Estos 3 archivos fueron integrados rápidamente a la rama principal usando Trunk-Based Development.');
 });
+
+document.body.innerHTML += "Aqui realizamos la actualización";
